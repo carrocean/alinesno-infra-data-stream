06eb774dd4026e07dfd657d1579cf35406aa4450
周健(James) <jamesAip@gmail.com>
2025-01-14 00:16:41 +0800

    refactor(DataStreamApplication.java): 移除不必要的注解并调整代码结构

--- alinesno-infra-data-stream-boot/src/main/java/com/alinesno/infra/data/stream/DataStreamApplication.java
+++ alinesno-infra-data-stream-boot/src/main/java/com/alinesno/infra/data/stream/DataStreamApplication.java
@@ -1,20 +1,15 @@
 package com.alinesno.infra.data.stream;
 
-import com.alinesno.infra.common.facade.enable.EnableActable;
-import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 集成一个Java开发示例工具
- * @author LuoAnDong
- * @since 2023年8月3日 上午6:23:43
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
  */
-@EnableActable
-@EnableInfraSsoApi
 @SpringBootApplication
-@MapperScan("com.alinesno.infra.data.stream.mapper")
 public class DataStreamApplication {
 
 	public static void main(String[] args) {
