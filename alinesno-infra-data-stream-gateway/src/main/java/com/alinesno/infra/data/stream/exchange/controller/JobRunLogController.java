0a677d465fb9c3ac3ebb65d854ef818ef8ed1c15
周健(James) <jamesAip@gmail.com>
2025-01-14 00:16:41 +0800

    fix:移除 JobRunLogController 中未使用的 ModelMap 引用

--- alinesno-infra-data-stream-gateway/src/main/java/com/alinesno/infra/data/stream/exchange/controller/JobRunLogController.java
+++ alinesno-infra-data-stream-gateway/src/main/java/com/alinesno/infra/data/stream/exchange/controller/JobRunLogController.java
@@ -24,7 +24,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
